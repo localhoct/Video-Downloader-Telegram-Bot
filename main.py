51fa1d04f1c3a8c42863509cedd0659e7ea86710
Yasin Javar | یاسین جاور <72578456+localhoct@users.noreply.github.com>
2021-01-07 11:28:48 +0330

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import youtube_dl
 from pyrogram import Client, filters
 from pyrogram.types import  InlineKeyboardMarkup, InlineKeyboardButton
 
-api_id = 1691381
-api_hash = "a4b862a161f7a320d5524730bbf51bf2"
-app = Client("Downlaoder", api_id, api_hash, bot_token='1597462763:AAErfe3g6-_WL1S1yVNctPN607bVBllxebI')
+api_id = 123456789
+api_hash = " Your Api Hash Here "
+app = Client("Downlaoder", api_id, api_hash, bot_token=' Your Bot Token here ')
 
 
 def downloada(url, quality):
@@ -25,12 +25,10 @@ def downloada(url, quality):
         }
         with youtube_dl.YoutubeDL(ydl_opts_start) as ydl:
             result = ydl.extract_info("{}".format(url))
-            # print(result)
             title = ydl.prepare_filename(result)
             ydl.download([url])
         return f'{title}'
     if quality == "2":
-        # print(99999999)
         ydl_opts_start = {
             'format': 'best[height=480]',
             'outtmpl': f'localhoct/%(title)s.%(ext)s',
@@ -60,21 +58,21 @@ async def webpage(c, m):
     sample_url = "https://da.gd/s?url={}".format(url1)
     url = requests.get(sample_url).text
     chat_id = m.chat.id
-    # await m.reply_text('you send url \n %s' % url)
+
     keys = await c.send_message(
-        chat_id,  # Edit this
+        chat_id,  
         "Okay!!🙄\n %s is Video Url😊 \n\nPlease Select Quality :\n 💡The HD Key is Download the Best Quality is available so I recommend This Key😁 " % url1,
         reply_markup=InlineKeyboardMarkup(
             [
-                [  # First row
-                    InlineKeyboardButton(  # Generates a callback query when pressed
+                [ 
+                    InlineKeyboardButton(  
                         "HD (Recommended)",
                         callback_data="%s and 1" % url
                     ),
 
                 ],
-                [  # Second row
-                    InlineKeyboardButton(  # Generates a callback query when pressed
+                [  
+                    InlineKeyboardButton(  
                         "SD (480p)",
                         callback_data="%s and 2" % url
                     ),
@@ -96,7 +94,6 @@ async def download(c, q):
     upmsg = await c.send_message(chat_id, 'Yeah😁 Uploading...')
     await dlmsg.delete()
     vid = await c.send_video(chat_id, path, caption='Downloaded by @iLoaderBot')
-    await vid.forward("iLoadeBot",as_copy=True)
     await upmsg.delete()
 
 
